1f5a6fb945893ed2064231e862a4853ad9aec426
octagony <moctagony@gmail.com>
2023-09-06 22:12:15 +0300

    [FEAT] Added modal component

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,8 +1,12 @@
+'use client'
+import { Modal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 export default function SetupPage() {
   return <div className='p-4'>
-    <UserButton afterSignOutUrl="/" />
+    <Modal isOpen title="Test" onClose={()=>{}} description="Test desc">
+      Children
+    </Modal>
   </div>;
 }
  
